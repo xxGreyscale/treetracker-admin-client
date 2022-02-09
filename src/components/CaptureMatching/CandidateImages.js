2179eefbe6bb15a65be17a428c7678cd58bc2e5e
xxGreyscale <salim.nassor.sn@gmail.com>
2022-02-09 13:42:10 +0300

    feat(candidateimages): accordion added for for candidate tree card

--- src/components/CaptureMatching/CandidateImages.js
+++ src/components/CaptureMatching/CandidateImages.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
-import { Typography, Box, Button, Grid, Paper } from '@material-ui/core';
+import {
+  Typography,
+  Box,
+  Button,
+  Grid,
+  Paper,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  AccordionActions,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import { makeStyles } from '@material-ui/core/styles';
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
@@ -10,13 +22,38 @@ const useStyles = makeStyles({
   containerBox: {
     marginTop: 0,
     marginBottom: theme.spacing(5),
-    paddingBottom: theme.spacing(2),
+    paddingBottom: theme.spacing(0),
     background: '#fff',
     borderRadius: '4px',
   },
 
   headerBox: {
     display: 'flex',
+  },
+
+  expanded: {},
+
+  headerBoxContent: {
+    margin: '0px',
+    '&$expanded': {
+      margin: '20px 0px 4px 4px',
+    },
+  },
+
+  headerBoxContentOnExpand: {
+    margin: '0px 8px',
+    '&$expanded': {
+      margin: '0px',
+    },
+  },
+
+  expandMoreIconWrapper: {
+    width: '1.5em',
+    height: '1.5em',
+  },
+
+  imgListContainer: {
+    padding: '0px 8px',
   },
 
   imgContainer: {
@@ -26,8 +63,9 @@ const useStyles = makeStyles({
     paddingBottom: '10px',
     overFlow: 'hidden',
   },
+
   gridList: {
-    padding: '10px',
+    padding: '10px 16px',
     display: 'flex',
     flexDirection: 'row',
     overflowX: 'auto',
@@ -37,6 +75,10 @@ const useStyles = makeStyles({
   imageScroll: {
     // height: '76vh',
     // overflow: 'scroll',
+  },
+
+  accordianAction: {
+    justifyContent: 'flex-start',
   },
 
   candidateImgBtn: {
@@ -93,12 +135,20 @@ function CandidateImages({ candidateImgData, sameTreeHandler }) {
       {candidateImgData &&
         candidateImgData.map((tree, i) => {
           return (
-            <Paper
-              elevation={4}
-              className={classes.containerBox}
-              key={`${i}-${tree.id}`}
-            >
-              <Box className={classes.headerBox}>
+            <Accordion className={classes.containerBox} key={`${i}-${tree.id}`}>
+              <AccordionSummary
+                expandIcon={
+                  <ExpandMoreIcon className={classes.expandMoreIconWrapper} />
+                }
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                IconButtonProps={{}}
+                classes={{
+                  content: classes.headerBoxContent,
+                  expanded: classes.expanded,
+                }}
+                className={classes.headerBox}
+              >
                 <Grid
                   container
                   className={classes.box2}
@@ -114,10 +164,10 @@ function CandidateImages({ candidateImgData, sameTreeHandler }) {
                   </Box>
                   <Box>{/* button */}</Box>
                 </Grid>
-              </Box>
+              </AccordionSummary>
 
               {showBox.includes(tree.id) ? (
-                <Box>
+                <AccordionDetails className={classes.imgListContainer}>
                   {tree.captures.length ? (
                     <Box className={classes.gridList} cols={3}>
                       {tree.captures.map((capture) => {
@@ -137,6 +187,7 @@ function CandidateImages({ candidateImgData, sameTreeHandler }) {
                     </Box>
                   ) : (
                     <Box
+                      className={classes.gridList}
                       style={{ height: '300px', color: 'blue' }}
                       key={tree.id}
                     >
@@ -147,33 +198,35 @@ function CandidateImages({ candidateImgData, sameTreeHandler }) {
                       />
                     </Box>
                   )}
-
-                  <Box className={classes.candidateImgBtn}>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      startIcon={<CheckIcon />}
-                      onClick={() => sameTreeHandler(tree.id)}
-                      style={{ color: 'white' }}
-                      className={classes.button}
-                    >
-                      Same Tree
-                    </Button>
-                    <Button
-                      id={tree.tree_id}
-                      variant="outlined"
-                      color="primary"
-                      startIcon={<ClearIcon />}
-                      onClick={() => hideImgBox(tree.id)}
-                      className={classes.button}
-                      value={i}
-                    >
-                      Different Tree
-                    </Button>
-                  </Box>
-                </Box>
+                </AccordionDetails>
               ) : null}
-            </Paper>
+
+              <AccordionActions className={classes.accordianAction}>
+                <Box className={classes.candidateImgBtn}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<CheckIcon />}
+                    onClick={() => sameTreeHandler(tree.id)}
+                    style={{ color: 'white' }}
+                    className={classes.button}
+                  >
+                    Same Tree
+                  </Button>
+                  <Button
+                    id={tree.tree_id}
+                    variant="outlined"
+                    color="primary"
+                    startIcon={<ClearIcon />}
+                    onClick={() => hideImgBox(tree.id)}
+                    className={classes.button}
+                    value={i}
+                  >
+                    Different Tree
+                  </Button>
+                </Box>
+              </AccordionActions>
+            </Accordion>
           );
         })}
     </Box>
